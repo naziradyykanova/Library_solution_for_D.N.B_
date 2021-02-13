b29aa88eaacb8c3909c3160c0585fb458d377ca5
zloisupport <kr0t77@yandex.ru>
2021-02-13 14:37:27 -0500

    + Авторы книг

---
+++ sql/Авторы книг.sql
@@ -0,0 +1,14 @@
+
+
+
+CREATE TABLE [dbo].[Авторы книг](
+	[Код записи] [int] NOT NULL,
+	[Шифр книги] [int] NULL,
+	[Код автора][int]  NULL,
+ CONSTRAINT [PK_Код Код записи] PRIMARY KEY CLUSTERED 
+(
+	[Код записи] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
